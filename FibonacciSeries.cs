d27020c37a2318e6d695c451d0a866a73bbcff77
singhtushar689 <singhtushar689@gmail.com>
2022-12-19 12:28:06 +0530

    Fibonacci Series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            Console.WriteLine("Enter the Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + " " + b + " ");
+            //Console.WriteLine(b + " ");
+            for(int i = 2; i <= number; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(b + " " );
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            fibonacci.Fibonacci();
         }
     }
 }
